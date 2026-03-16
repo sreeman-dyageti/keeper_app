4d0b82cf802dc435d44f130ab5a331d924cebb84
Dyageti.Sai Sreeman <260058045+sreeman-dyageti@users.noreply.github.com>
2026-03-16 22:12:17 +0530

    feat: Add header, footer, note, app components to main.jsx with styles

---
+++ src/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>{year}</p>
+    </footer>
+  );
+}
---
+++ src/note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function note(){
+    return <div className="note">
+        <h1>title</h1>
+        <p>content</p>
+    </div>
+}
+
+export default note;
