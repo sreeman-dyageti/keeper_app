41e031ce620fa23af6480a33b4ae31247b3dc866
Dyageti.Sai Sreeman <260058045+sreeman-dyageti@users.noreply.github.com>
2026-03-20 19:56:48 +0530

    feat: Applied mapping throught the notes array

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "./notes";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
-      <main>
-        <Note />
+      <main>  
+       {notes.map((note) => {
+            return <Note 
+            key={note.id}
+            title={note.title}
+            content={note.content}
+            />
+          })} 
       </main>
       <Footer />
     </div>
